e0246ba8fb818de0947a4fc012768c26a7281992
Szymon Sulejczak <55898132+Sul3j@users.noreply.github.com>
2021-03-05 10:32:23 +0100

    ADD: Winning conditions array

--- src/script.js
+++ src/script.js
@@ -1,6 +1,16 @@
 var fieldsElements = document.querySelectorAll('.board-item');
 var fields = ['', '', '', '', '', '', '', '', ''];
 var activePlayer = 'X';
+var winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+];
 fieldsElements.forEach(function (field) {
     field.addEventListener('click', function (e) {
         var target = e.target;
--- src/script.ts
+++ src/script.ts
@@ -4,6 +4,17 @@ const fields: string[] = ['','','','','','','','','']
 
 let activePlayer: string = 'X';
 
+const winningConditions: Array<number[]> = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+]
+
 fieldsElements.forEach((field) => {
     field.addEventListener('click', (e) => {
 
